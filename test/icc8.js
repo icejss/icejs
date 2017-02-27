6b945e0d5f4dc4cb2c69ee47d03ac188a19fa0cd
icejs <sxgyyyxh@126.com>
2017-02-27 17:45:18 +0800

    创建icejs工程,首次提交.

---
+++ test/icc8.js
@@ -0,0 +1,31 @@
+location.ancestorOrigins
+DOMStringList {}
+
+location.assign
+() { [native code] }
+location.reload
+reload() { [native code] }
+location.replace
+() { [native code] }
+location.toString
+toString() { [native code] }
+location.valueOf
+valueOf() { [native code] }
+location.hash
+""
+location.host
+""
+location.hostname
+""
+location.href
+"file:///D:/workspace/icejs/src/lodash.html"
+location.origin
+"file://"
+location.pathname
+"/D:/workspace/icejs/src/lodash.html"
+location.port
+""
+location.protocol
+"file:"
+location.search
+""
